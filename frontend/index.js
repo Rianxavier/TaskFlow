c8f940f9c2d4b4c136e1f489a0bac7a670b20e41
Rianxavier <rianalvesxvr@gmail.com>
2023-07-28 00:07:37 -0300

    feat: update login

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,12 @@
+const btnVerSenha = document.querySelector('#verSenha');
+const inputSenha = document.querySelector("#senha");
 
+btnVerSenha.addEventListener("click", verSenha);
+
+function verSenha() {
+  if (inputSenha.getAttribute("type") == "password") {
+    inputSenha.setAttribute("type", "text");
+  } else {
+    inputSenha.setAttribute("type", "password");
+  }
+}
